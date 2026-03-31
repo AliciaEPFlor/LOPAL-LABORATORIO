78ff00ecf117590d1b0da7f748d933906858d4b5
AliciaEPFlor <aliciapflor10@gmail.com>
2026-03-31 16:46:12 -0300

    first commit

---
+++ operacoes_math.py
@@ -0,0 +1,16 @@
+
+# soma = 10 + 10
+# sub = 50 - 50
+# mult = 10 * 2
+# div = 100 / 10
+# #Isto o python não executa
+# print(f"Valor do soma é:{soma}")
+# print(f"Valor da subtração e:{sub}")
+# print(f"Valor da multiplicação é:{mult}")
+# print(f"Valor da divisão é:{div}")
+
+nome = input("Digite seu nome:")
+print(f"Ola {nome}")
+
+
+
