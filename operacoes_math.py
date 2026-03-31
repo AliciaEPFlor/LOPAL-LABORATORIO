83ec99c455eef0f4d181662a19238836b69f33ee
AliciaEPFlor <aliciapflor10@gmail.com>
2026-03-31 17:13:21 -0300

    Digitação de codigos para soma, multiplicação, divisão e subtração, fazendo tambem o resto inteiro e apenas o resto da divisão

--- operacoes_math.py
+++ operacoes_math.py
@@ -7,6 +7,10 @@ rest_div = 5 % 3
 div_int = 5 // 3
 print(f"O resto da divisão é:{rest_div}")
 print(f"O resto da divisão inteira é:{div_int}")
+print(f"O resto da divisão fração é:{div_int} {rest_div}/3")
+
+
+
 # # #Isto o python não executa
 # print(f"Valor do soma é:{soma}")
 # print(f"Valor da subtração e:{sub}")
